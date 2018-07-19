b682dab4e75aa7657abba5513425b5c33e71fbce
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-19 17:52:20 +0000

    automatically backed up by learn

---
+++ db/migrate/07_remove_column.rb
@@ -0,0 +1,9 @@
+
+class AddChildren < ActiveRecord::Migration[4.2]
+  def change
+    remove_column :artists, :favorite_food
+    remove_column :artists, :favorite_color
+    remove_column :artists, :children
+  end
+
+end
