fb1563bd19021c5d78e0ae1e241957d564d9d96f
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-19 17:35:58 +0000

    automatically backed up by learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -2,7 +2,7 @@ class AddFavoriteFoodToArtists < ActiveRecord::Migration[4.2]
   def change
     add_column :artists, :favorite_food, :string
   end
-  
+
   def other_change
     add_column :artists, :gender, :string
   end
